be4e1a3a77c7d6fadb20c1ae09e42522bada12df
gxc1227 <gxc19851227@163.com>
2025-03-25 09:38:28 +0800

    菜单项目添加

--- manager/vue/src/views/Manager.vue
+++ manager/vue/src/views/Manager.vue
@@ -33,7 +33,8 @@
     <div class="manager-main">
       <!--  侧边栏  -->
       <div class="manager-main-left">
-        <el-menu :default-openeds="['info', 'user']" router style="border: none" :default-active="$route.path">
+        <!-- 将 default-openeds 属性的值设为空数组 -->
+        <el-menu :default-openeds="[]" router style="border: none" :default-active="$route.path">
           <el-menu-item index="/home">
             <i class="el-icon-s-home"></i>
             <span slot="title">系统首页</span>
@@ -44,12 +45,54 @@
             </template>
             <el-menu-item index="/notice">公告信息</el-menu-item>
           </el-submenu>
-
-          <el-submenu index="user">
+          <el-submenu index="system">
             <template slot="title">
-              <i class="el-icon-menu"></i><span>用户管理</span>
+              <i class="el-icon-menu"></i><span>系统管理</span>
             </template>
-            <el-menu-item index="/admin">管理员信息</el-menu-item>
+            <el-menu-item index="/admin">管理员管理</el-menu-item>
+            <el-menu-item index="/user">用户管理</el-menu-item>
+            <el-menu-item index="/role">角色管理</el-menu-item>
+            <el-menu-item index="/permission">权限管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="course">
+            <template slot="title">
+              <i class="el-icon-menu"></i><span>课程管理</span>
+            </template>
+            <el-menu-item index="/course-create">课程创建</el-menu-item>
+            <el-menu-item index="/task-issue">任务下发</el-menu-item>
+            <el-menu-item index="/course-activate">课程激活</el-menu-item>
+          </el-submenu>
+          <el-submenu index="assessment">
+            <template slot="title">
+              <i class="el-icon-menu"></i><span>评估管理</span>
+            </template>
+            <el-menu-item index="/assessment-dimension">评估维度管理</el-menu-item>
+            <el-menu-item index="/assessment-task">评估任务管理</el-menu-item>
+            <el-menu-item index="/assessment-result">评估结果查看</el-menu-item>
+          </el-submenu>
+          <el-submenu index="score">
+            <template slot="title">
+              <i class="el-icon-menu"></i><span>成绩管理</span>
+            </template>
+            <el-menu-item index="/score-input">成绩录入</el-menu-item>
+            <el-menu-item index="/score-query">成绩查询</el-menu-item>
+            <el-menu-item index="/score-statistics">成绩统计</el-menu-item>
+          </el-submenu>
+          <el-submenu index="comprehensive">
+            <template slot="title">
+              <i class="el-icon-menu"></i><span>综测管理</span>
+            </template>
+            <el-menu-item index="/material-upload">材料上传</el-menu-item>
+            <el-menu-item index="/score-feedback">成绩反馈</el-menu-item>
+            <el-menu-item index="/proof-audit">证明审核</el-menu-item>
+          </el-submenu>
+          <el-submenu index="analysis">
+            <template slot="title">
+              <i class="el-icon-menu"></i><span>数据分析</span>
+            </template>
+            <el-menu-item index="/academic-analysis">学业分析</el-menu-item>
+            <el-menu-item index="/comprehensive-analysis">综测分析</el-menu-item>
+            <el-menu-item index="/comprehensive-report">综合报告</el-menu-item>
           </el-submenu>
         </el-menu>
       </div>
